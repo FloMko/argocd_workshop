4f7fa39d914b64e9abcd62293e2cdddb055ae808
Maksim Ermolenko <maksim.ermolenko@softswiss.com>
2022-12-02 15:26:41 +0400

    problemn with env

--- packaging/apps/flask-hello/app.py
+++ packaging/apps/flask-hello/app.py
@@ -4,8 +4,8 @@ import os
 app = Flask(__name__)
 
 argo_env = os.environ.get("ARGO_ENV")
-argo_env2 = os.environ.get("ARGO_ENV2")
-argo_env3 = os.environ.get("ARGO_ENV3")
+argo_env2 = os.environ.get("ARGO_ENV_2")
+argo_env3 = os.environ.get("ARGO_ENV_3")
 
 
 @app.route('/')
